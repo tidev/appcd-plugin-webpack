70b0d540783f12d5ee3e6736eedd729951fff2f9
Jan Vennemann <jan.vennemann@gmx.net>
2019-10-10 14:09:05 +0200

    fix: run initial appcd status check only during ssr

--- app/middleware/online-check.js
+++ app/middleware/online-check.js
@@ -1,17 +1,13 @@
 export default async function({ $axios, from, redirect, route, store }) {
   if (process.client) {
-    if (!store.state.webpack.connected) {
-      redirect(from)
-    }
-  } else {
-    try {
-      await $axios.get('/appcd/status')
-      if (route.path === '/offline') {
-        redirect(route.query.redirect || '/')
-      }
-    } catch (e) {
-      const redirectTo = route.query.redirect || route.path
-      redirect('/offline', { redirect: redirectTo })
-    }
+    return
+  }
+
+  try {
+    await $axios.get('/appcd/status')
+    store.commit('webpack/connected', true)
+  } catch (e) {
+    const redirectTo = route.query.redirect || route.path
+    redirect('/offline', { redirect: redirectTo })
   }
 }
