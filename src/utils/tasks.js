7fca2c128f5f55fd870f5cfc841a3eff8d7ceff2
Jan Vennemann <jan.vennemann@gmx.net>
2019-10-23 15:42:36 +0200

    fix: resolve new module option

--- src/utils/tasks.js
+++ src/utils/tasks.js
@@ -1,8 +1,19 @@
 export function resolveArgs() {
 	const rawArgv = process.argv.slice(2);
-	return require('minimist')(rawArgv, {
+	const parsedArgs = require('minimist')(rawArgv, {
+		alias: {
+			modules: 'm'
+		},
 		boolean: [
 			'watch'
-		]
+		],
+		default: {
+			modules: []
+		}
 	});
+	if (!Array.isArray(parsedArgs.modules)) {
+		parsedArgs.modules = [parsedArgs.modules];
+	}
+	return parsedArgs;
 }
+
