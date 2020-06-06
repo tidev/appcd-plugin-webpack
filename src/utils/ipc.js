7775e2caf0bc2644ccf5ba857675972dc95c1fc6
Jan Vennemann <jan.vennemann@gmx.net>
2020-06-06 03:57:34 +0200

    fix: ipc flow control

--- src/utils/ipc.js
+++ src/utils/ipc.js
@@ -1,3 +1,19 @@
+const queue = [];
+let sendMore = true;
+
+const sendQueue = () => {
+	if (queue.length === 0) {
+		sendMore = true;
+		return;
+	}
+	const message = queue.shift();
+	sendMore = process.send(message, sendQueue);
+};
+
 export function sendData(type, data) {
-	process.send && process.send({ type, data });
+	if (!sendMore) {
+		queue.push({ type, data, time: Date.now() });
+		return;
+	}
+	sendMore = process.send({ type, data, time: Date.now() }, sendQueue);
 }
